361db6c70671a60020ee778c0062a968057b1d51
vishnuprasad <vishnuprasad.p2006@gmail.com>
2026-06-22 16:07:16 +0530

    Added expense and category arguments to CLI

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+    description="Finance Tracker CLI"
+)
 
 parser.add_argument(
     "--income",
     type=float,
-    help="Add income"
+    help="Add income amount"
+)
+
+parser.add_argument(
+    "--expense",
+    type=float,
+    help="Add expense amount"
+)
+
+parser.add_argument(
+    "--category",
+    type=str,
+    help="Expense category"
 )
 
 args = parser.parse_args()
 
 if args.income:
-    print(
-        f"Income Added: ₹{args.income}"
-    )
+    print(f"Income Added: ₹{args.income}")
+
+if args.expense:
+    print(f"Expense Added: ₹{args.expense}")
+
+if args.category:
+    print(f"Category: {args.category}")
